f1b4c349f60505050a7256cc06902768a6b762ad
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-08-20 21:44:23 +0200

    init

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	discord, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	discord.AddHandler(ready)
+	discord.AddHandler(interactionCreate)
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "test",
+			Description: "A test command.",
+		},
+	}
+
+	for _, guild := range event.Guilds {
+		_, err := s.ApplicationCommandCreate(s.State.User.ID, guild.ID, commands[0])
+		if err != nil {
+			fmt.Println("error creating command,", err)
+			return
+		}
+	}
+}
+
+func interactionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type == discordgo.InteractionApplicationCommand {
+		if i.ApplicationCommandData().Name == "test" {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Test recieved!",
+				},
+			})
+		}
+	}
+}
